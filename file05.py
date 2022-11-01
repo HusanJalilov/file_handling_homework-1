42cd28bb25f52c5ac21e248af74a94b6ea9ceb67
coder_uz <husanjalilov00777@gmil.com>
2022-11-01 19:53:41 +0500

    05

--- file05.py
+++ file05.py
@@ -6,5 +6,17 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    s=0
+    m=0
+    for i in data:
+        if not i.isdigit():
+            s+=1
+        else:
+            m+=1
+    return [m,s]
+
     
 # Read data from file
+
+data=open('txt_file/data05.txt','r').read()
+print(main(data))
