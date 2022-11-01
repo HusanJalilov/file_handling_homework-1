54d675c3399edf5c20a9927bf7d575552b4dec4e
coder_uz <husanjalilov00777@gmil.com>
2022-11-01 20:00:13 +0500

    06

--- file06.py
+++ file06.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    s=data.readlines()
+    m=[]
+    for i in s:
+        m.append(len(i)-1)
+    return m
+
     
 # Read data from file
+
+data=open('txt_file/data06.txt','r')
+print(main(data))
